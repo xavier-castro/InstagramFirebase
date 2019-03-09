409cfa7f9dd24425ee0ef1d620c1ae1d6bc7a762
xavier-castro <xavier_castro@icloud.com>
2019-03-08 23:22:38 -0600

    Add Registration fields

---
+++ InstagramFirebase/UIColor+AnchorExtension.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor+Anchor.swift
+//  InstagramFirebase
+//
+//  Created by Xavier Castro on 3/8/19.
+//  Copyright © 2019 Xavier Castro. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: alpha)
+    }
+
+}
--- InstagramFirebase/ViewController.swift
+++ InstagramFirebase/ViewController.swift
@@ -12,15 +12,88 @@ class ViewController: UIViewController {
 
     let plusPhotoButton: UIButton = {
         let button = UIButton(type: .system)
-        button.backgroundColor = .red
+        button.setImage(#imageLiteral(resourceName: "plus_photo").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    let emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Email"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        textField.borderStyle = .roundedRect
+        textField.font = UIFont.systemFont(ofSize: 14)
+        return textField
+    }()
+
+    let usernameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Username"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        textField.borderStyle = .roundedRect
+        textField.font = UIFont.systemFont(ofSize: 14)
+        return textField
+    }()
+
+    let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Password"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        textField.borderStyle = .roundedRect
+        textField.font = UIFont.systemFont(ofSize: 14)
+        textField.isSecureTextEntry = true
+        return textField
+    }()
+
+    let signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign Up", for: .normal)
+        button.backgroundColor = UIColor.rgb(red: 149, green: 204, blue: 244, alpha: 1)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+        button.setTitleColor(.white, for: .normal)
         return button
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        view.backgroundColor = .white
+
         view.addSubview(plusPhotoButton)
-        plusPhotoButton.frame = .init(x: 0, y: 0, width: 250, height: 250)
+        plusPhotoButton.heightAnchor.constraint(equalToConstant: 140).isActive = true
+        plusPhotoButton.widthAnchor.constraint(equalToConstant: 140).isActive = true
+        plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        plusPhotoButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
+
+        setupInputFields()
+
+    }
+
+    fileprivate func setupInputFields() {
+
+        let stackView = UIStackView(arrangedSubviews: [
+            emailTextField,
+            usernameTextField,
+            passwordTextField,
+            signUpButton
+            ])
+
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        stackView.spacing = 10
+
+        view.addSubview(stackView)
+        stackView.topAnchor.constraint(equalTo: plusPhotoButton.bottomAnchor, constant: 20).isActive = true
+        stackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
+        stackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40).isActive = true
+        stackView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+
+
     }
 
 
