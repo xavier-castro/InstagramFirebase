0d5d4912d8b2cfad5a75140943b8f60623cc1be9
xavier-castro <xavier_castro@icloud.com>
2019-04-08 16:53:57 -0500

    Add custom delegation

---
+++ InstagramFirebase/Home/CommentsController.swift
@@ -0,0 +1,18 @@
+//
+//  CommentsController.swift
+//  InstagramFirebase
+//
+//  Created by Xavier Castro on 4/8/19.
+//  Copyright © 2019 Xavier Castro. All rights reserved.
+//
+
+import UIKit
+
+class CommentsController: UICollectionViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		collectionView.backgroundColor = .red
+	}
+	
+}
--- InstagramFirebase/Home/HomeController.swift
+++ InstagramFirebase/Home/HomeController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Firebase
 
-class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout, HomePostCellDelegate {
 
 	let cellId = "cellId"
 
@@ -127,7 +127,15 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
 	override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! HomePostCell
 		cell.post = posts[indexPath.item]
+		cell.delegate = self
 		return cell
+	}
+
+	func didTapComment(post: Post) {
+		print("Message coming from HomeController")
+		print(post.caption)
+		let commentsController = CommentsController(collectionViewLayout: UICollectionViewFlowLayout())
+		navigationController?.pushViewController(commentsController, animated: true)
 	}
 
 }
